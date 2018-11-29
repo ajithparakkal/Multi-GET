98e6685891d66af90026f55fe58e5da3d29562db
Ajith Balakrishnan <ajithparakkal@hotmail.com>
2018-11-29 17:36:02 -0500

    adding build and script files

---
+++ Multi-GET.py
@@ -0,0 +1,40 @@
+# Script that downloads part of a file from a web server, in chunks.
+# reference URL for HTTP requests{'http://docs.python-requests.org/en/latest/'}
+
+import requests, os
+
+# let the user choose an input URL
+url = input("Enter the download URL: ")
+
+# Function to download files in multiple Chunks
+def file_download(url, output_path):
+    # HTTP range function to request only first 4MiB of the file
+    headers = {'Range': 'bytes=0-3999999'} 
+    try:
+        # get request for the URL
+        response = requests.get(url, headers=headers)
+    except requests.exceptions.MissingSchema:
+        #exit if the URL is invalid
+        print("Invalid URL")
+        exit()
+        
+    #creating output file name from url and output path and appending 'part1' to it
+    file_name = os.path.basename(url) + ".part1"
+    output_file_name = os.path.join(output_path, file_name)
+    output_file = open(output_file_name, "wb")
+
+    ## loop to download the file in multiple chunks of 1MiB size from response
+    for chunk in response.iter_content(chunk_size=1000000):
+        if chunk:
+            output_file.write(chunk)
+    output_file.close() 
+
+# let the user choose an output file path
+output_path = input("Enter path for saving downloaded file: ")
+if os.path.exists(output_path):
+    file_download(url, output_path)
+else:
+    print("Path doesnot exist")
+    exit()
+
+
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Multi-GET
+#App that downloads part of a file from a web server, in chunks.
+
+#Language:
+Python 3.6.7
+
+#Build details(OS):
+macosx-10.9-x86_64
+
+#Specific App Requirements added:
+
+• Source URL should be specified with a required command-line option
+• File is downloaded in 4 chunks (4 requests made to the server)
+• Only the first 4 MiB of the file should be downloaded from the server
+• Output file may be specified with a command-line option
+• No corruption in file - correct order, correct size, correct bytes
+• File is retrieved with GET requests
+
+
+#References:
+http://docs.python-requests.org/en/latest/
+
