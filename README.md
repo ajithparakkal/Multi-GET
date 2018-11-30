7571db361c2ce2388629ea5cf6fb5dc09a938452
Ajith Balakrishnan <ajithparakkal@hotmail.com>
2018-11-29 21:31:31 -0500

    changed units to MiB from MB and user input for chunks, chunk size and download size

--- README.md
+++ README.md
@@ -9,14 +9,19 @@ macosx-10.9-x86_64
 
 #Specific App Requirements added:
 
-• Source URL should be specified with a required command-line option
-• File is downloaded in 4 chunks (4 requests made to the server)
-• Only the first 4 MiB of the file should be downloaded from the server
-• Output file may be specified with a command-line option
-• No corruption in file - correct order, correct size, correct bytes
-• File is retrieved with GET requests
+1. Source URL should be specified with a required command-line option
+2. File is downloaded in 4 chunks (4 requests made to the server)
+3. Only the first 4 MiB of the file should be downloaded from the server
+4. Output file may be specified with a command-line option
+5. No corruption in file - correct order, correct size, correct bytes
+6. File is retrieved with GET requests
+7. Support files smaller than 4 MiB (less chunks/adjust chunk size)
+8. Configurable number of chunks/chunk size/total download size
+
+#Test URL:
+http://f39bf6aacedfcf.bwtest-aws.pravala.com/384MB.jar
 
 
 #References:
-http://docs.python-requests.org/en/latest/
+http://docs.python-requests.org/
 
--- dist/Multi-GET.app/Contents/Resources/Multi-GET.py
+++ dist/Multi-GET.app/Contents/Resources/Multi-GET.py
@@ -1,40 +1,56 @@
 # Script that downloads part of a file from a web server, in chunks.
-# reference URL {'http://docs.python-requests.org/en/latest/'}
+# reference URL used for HTTP requests {'http://docs.python-requests.org/'}
 
-import requests, os
+import requests, os, sys
 
 # let the user choose an input URL
 url = input("Enter the download URL: ")
 
 # Function to download files in multiple Chunks
-def file_download(url, output_path):
+def file_download(url, output_path, chunk_size_bytes, total_size):
     # HTTP range function to request only first 4MiB of the file
-    headers = {'Range': 'bytes=0-3999999'} 
+    total_size = str(total_size-1) #converting to string for adding range
+    headers = {'Range': 'bytes=0-'+total_size}
     try:
         # get request for the URL
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, stream=True, headers=headers)
     except requests.exceptions.MissingSchema:
         #exit if the URL is invalid
         print("Invalid URL")
-        exit()
+        sys.exit()
         
-    #creating output file name from url and output path and appending 'part1' to it
+    #creating output file name from url and output path
     file_name = os.path.basename(url) + ".part1"
     output_file_name = os.path.join(output_path, file_name)
     output_file = open(output_file_name, "wb")
 
-    ## loop to download the file in multiple chunks of 1MiB size from response
-    for chunk in response.iter_content(chunk_size=1000000):
+    ## loop to download the file in multiple chunks of 1MiB size
+    
+    for chunk in response.iter_content(chunk_size=chunk_size_bytes):
         if chunk:
             output_file.write(chunk)
     output_file.close() 
 
+#let the user choose 'no of chunks and chunk size'
+input_string = "Enter no of chunks and chunksize(MiB) seperated by commas: "
+try:
+    chunk_no, chunk_size = tuple(map(int,input(input_string).split(',')))
+except ValueError:
+    #exit program if non integer values are entered
+    print("Wrong values entered for chunks and/or chunksizes")
+    sys.exit()
+
+#calculating the chunk sizes(bytes) and total file size(bytes) for download    
+chunk_size_bytes = chunk_size*1024*1024
+total_size = chunk_size_bytes*chunk_no
+
 # let the user choose an output file path
 output_path = input("Enter path for saving downloaded file: ")
 if os.path.exists(output_path):
-    file_download(url, output_path)
+    file_download(url, output_path, chunk_size_bytes, total_size)
 else:
+    #exit program if path doesnot exist
     print("Path doesnot exist")
-    exit()
+    sys.exit()
 
 
